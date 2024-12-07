fc496b0179901672c4e643d88133cec01d7fcdca
artemenko_pa <artemenko_pa@magnit.ru>
2024-12-07 14:59:24 +0300

    Add weak pointer ability to object

--- SpaceShooterEngine/include/framework/Object.h
+++ SpaceShooterEngine/include/framework/Object.h
@@ -1,13 +1,18 @@
 #pragma once
+#include <memory>
+
+#include "Core.h"
 
 namespace SpaceShooter {
-    class Object {
+    class Object : public std::enable_shared_from_this<Object> {
     public:
         Object() = default;
         virtual ~Object() = default;
 
         virtual void Destroy();
         [[nodiscard]] bool IsPendingDestruction() const;
+        Weak<Object> GetWeakPtr() { return shared_from_this(); }
+        Weak<const Object> GetWeakPtr() const { return shared_from_this(); }
 
     private:
         bool isPendingDestruction = false;
