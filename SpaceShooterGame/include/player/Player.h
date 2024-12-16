5e5da99f1b6696dda77fd61e12f684bf89ed845d
artemenko_pa <artemenko_pa@magnit.ru>
2024-12-16 15:23:16 +0300

    Add player class

---
+++ SpaceShooterGame/include/player/Player.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "framework/Delegate.h"
+#include "framework/Object.h"
+
+
+namespace SpaceShooter {
+    class PlayerSpaceShip;
+    class World;
+
+    class Player final : public Object {
+    public:
+        explicit Player() = default;
+
+        Weak<PlayerSpaceShip> SpawnSpaceship(World *world);
+        Weak<PlayerSpaceShip> GetSpaceship() const { return spaceShip; }
+
+        void AddLifeCount(unsigned int count);
+        unsigned int GetLifeCount() const { return lifeCount; }
+
+        void AddScore(unsigned int score);
+        unsigned int GetScore() const { return score; }
+
+        Delegate<unsigned int> LifeCountChanged;
+        Delegate<unsigned int> ScoreChanged;
+        Delegate<> LifeCountDepleted;
+
+    private:
+        unsigned int lifeCount{3};
+        unsigned int score{0};
+        Weak<PlayerSpaceShip> spaceShip{};
+    };
+}
---
+++ SpaceShooterGame/src/player/Player.cpp
@@ -0,0 +1,34 @@
+#include "player/Player.h"
+
+#include "framework/World.h"
+#include "player/PlayerSpaceShip.h"
+
+
+namespace SpaceShooter {
+    Weak<PlayerSpaceShip> Player::SpawnSpaceship(World *world) {
+        if (lifeCount > 0) {
+            --lifeCount;
+            const auto [windowWidth, windowHeight] = world->GetWindowSize();
+            spaceShip = world->SpawnActor<PlayerSpaceShip>();
+            spaceShip.lock()->SetPosition({windowWidth * 0.5f, windowHeight * 0.9f});
+            LifeCountChanged.Emit(lifeCount);
+        } else {
+            LifeCountDepleted.Emit();
+        }
+        return spaceShip;
+    }
+
+    void Player::AddLifeCount(const unsigned int count) {
+        if (count == 0) return;
+
+        lifeCount += count;
+        LifeCountChanged.Emit(lifeCount);
+    }
+
+    void Player::AddScore(const unsigned int score) {
+        if (score == 0) return;
+
+        this->score += score;
+        ScoreChanged.Emit(this->score);
+    }
+}
